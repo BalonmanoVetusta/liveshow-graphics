47685733f4f9e19c0560fda26f0109b5e67bb495
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2022-08-19 15:37:59 +0200

    postinstall script is now capable of modify package.json of nodecg to use bun instead of node

--- bin/postinstall.mjs
+++ bin/postinstall.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { exec } from "node:child_process";
-import { existsSync } from "node:fs";
+import { existsSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 
 if (
@@ -11,9 +11,31 @@ if (
 
 const PROJECT_PATH_PREFIX = process.env.npm_config_local_prefix;
 const NODECG_PROJECT_PATH = join(PROJECT_PATH_PREFIX, ".nodecg");
+const PACKAGE_JSON_FILEPATH = join(NODECG_PROJECT_PATH, "package.json");
+
+function modifyNodeCGPackageJSONForBun(packageJsonFilePath) {
+  try {
+    const packageJSON = require(packageJsonFilePath);
+
+    Array.from(Object.keys(packageJSON.scripts)).forEach((key, index) => {
+      console.log({ key, value: packageJSON.scripts[key] });
+      packageJSON.scripts[key] = packageJSON.scripts[key].replaceAll(
+        "node ",
+        "bun "
+      );
+      packageJSON.scripts[key] = packageJSON.scripts[key].replaceAll(
+        "npm ",
+        "bun "
+      );
+    });
+    writeFileSync(packageJsonFilePath, JSON.stringify(packageJSON, null, 2));
+  } catch (error) {
+    console.error("Could not modify package.json of .nodecg to use Bun");
+  }
+}
 
 if (!existsSync(NODECG_PROJECT_PATH)) {
-  console.log('Installing nodecg')
+  console.log("Installing nodecg");
   exec("include-nodecg postinstall", (err, stdout, stderr) => {
     if (err) {
       console.error({ err });
@@ -28,4 +50,14 @@ if (!existsSync(NODECG_PROJECT_PATH)) {
   });
 } else {
   console.log("nodecg already installed, ignoring the postinstall process");
+  if (Boolean(process?.isBun)) {
+    console.log("Modifying the package.json of .nodecg to use Bun");
+    modifyNodeCGPackageJSONForBun(PACKAGE_JSON_FILEPATH);
+    console.log("Done");
+    process.exit();
+  }
+}
+
+if (process.argv.includes("--bun")) {
+  modifyNodeCGPackageJSONForBun(PACKAGE_JSON_FILEPATH);
 }
