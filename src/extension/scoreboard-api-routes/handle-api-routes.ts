2c4707361166c81df0038c75c4df46d2264dc5ee
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2022-10-17 01:09:11 +0200

    Added API to know the current score

--- src/extension/scoreboard-api-routes/handle-api-routes.ts
+++ src/extension/scoreboard-api-routes/handle-api-routes.ts
@@ -1,12 +1,42 @@
 import { randomUUID } from "crypto";
+import { MatchActions } from "types/schemas/match-actions";
 import { scoreboardActions } from "./scoreboard-actions";
 import { NodeCG } from "/.nodecg/types/server";
-import { Team } from "/src/hooks/use-match-actions/types";
+import { MatchActionType, Team } from "/src/hooks/use-match-actions/types";
+
+const MATCH_ACTIONS_REPLICANT_NAME = "match-actions";
 
 export function handleApiRoutes(nodecg: NodeCG) {
   const router = nodecg.Router();
 
   const { removeLastGoal, addGoal } = scoreboardActions(nodecg, randomUUID);
+
+  const actions = nodecg.Replicant<MatchActions>(
+    MATCH_ACTIONS_REPLICANT_NAME,
+    nodecg.bundleName,
+    {
+      defaultValue: [],
+      persistent: true,
+    }
+  );
+
+  router.get("/:team/goals", (req, res) => {
+    const team = (req.params.team as Team).toUpperCase();
+
+    if (team === undefined || team === null) {
+      return res.status(400).json({ error: "team is required" });
+    }
+
+    const result = actions.value.filter(
+      (action) =>
+        action.team.toUpperCase() === team &&
+        action.action === MatchActionType.GOAL
+    );
+
+    return res.json({
+      payload: { actions: result, total: result.length },
+    });
+  });
 
   router.get("/:team/add", (req, res) => {
     try {
