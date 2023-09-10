8bf297a3b7bad7b32d3a01bfc2959307b34db53b
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-10 03:36:43 +0200

    Fix building

--- Dockerfile
+++ Dockerfile
@@ -9,18 +9,12 @@ USER nodecg
 
 COPY --chown=nodecg:nodecg cfg/nodecg* cfg/
 COPY --chown=nodecg:nodecg package.json bundles/${NODECG_BUNDLE_NAME}/package.json
-COPY --chown=nodecg:nodecg package-lock.json bundles/${NODECG_BUNDLE_NAME}/package-lock.json
 COPY --chown=nodecg:nodecg cfg bundles/${NODECG_BUNDLE_NAME}/cfg
 COPY --chown=nodecg:nodecg images bundles/${NODECG_BUNDLE_NAME}/images
 COPY --chown=nodecg:nodecg schemas bundles/${NODECG_BUNDLE_NAME}/schemas
 COPY --chown=nodecg:nodecg dashboard bundles/${NODECG_BUNDLE_NAME}/dashboard
 COPY --chown=nodecg:nodecg graphics bundles/${NODECG_BUNDLE_NAME}/graphics
 COPY --chown=nodecg:nodecg extension bundles/${NODECG_BUNDLE_NAME}/extension
-# WORKDIR /opt/nodecg/bundles/${NODECG_BUNDLE_NAME}
-# COPY --chown=nodecg:nodecg src src
-# RUN npm install
-# RUN npm run build
-# RUN rm -rf node_modules src
 
 WORKDIR /opt/nodecg/
 VOLUME /opt/nodecg/cfg /opt/nodecg/bundles /opt/nodecg/logs /opt/nodecg/db /opt/nodecg/assets
--- cfg/nodecg.js
+++ cfg/nodecg.js
@@ -1,58 +1,24 @@
 // import { randomBytes } from "node:crypto";
 
+const { env = {} } = process ?? {};
+
 const config = {
   host: "0.0.0.0",
   port: 9090,
-  developer: true,
   login: {
     enabled: false,
-    sessionSecret: process.env.NODECG_SESSION_SECRET ?? "notsecure", // randomBytes(128).toString("hex"),
+    sessionSecret: env.NODECG_SESSION_SECRET ?? "notsecure", // randomBytes(128).toString("hex"),
   },
-  logging: {
-    replicants: false,
-    console: {
-      enabled: true,
-      timestamps: false,
-      level: "trace",
-    },
-    file: {
-      enabled: true,
-      timestamps: true,
-      path: "logs/server.log",
-      level: "info",
-    },
-  },
-  //   ssl: {
-  //     enabled: false,
-  //     keyPath: "",
-  //     certificatePath: "",
-  //   },
-  //   sentry: {
-  //     enabled: false,
-  //     dsn: "https://xxx:yyy@sentry.io/zzz",
-  //     publicDsn: "https://xxx@sentry.io/zzz",
-  //   },
 };
 
-// Check if we are in production mode
-if (!process.env.NODE_ENV?.toLowerCase().startsWith("prod")) {
-  config.developer = true;
-  config.login.enabled = false;
-  config.login.twitch.enabled = false;
-  config.login.steam.enabled = false;
-  config.logging.console.level = "debug";
-  config.logging.file.level = "debug";
-  config.sentry.enabled = false;
-}
-
-if (!config.developer) {
-  if (process.env.NODECG_HOST) {
-    config.host = process.env.NODECG_HOST;
+if (env.NODE_ENV?.toLowerCase().startsWith("prod")) {
+  if (env.NODECG_HOST) {
+    config.host = env.NODECG_HOST;
   }
 
   // Basic auth
-  if (process.env.NODECG_BASIC_USERS) {
-    const allowedUsers = process.env.NODECG_BASIC_USERS.split(",")
+  if (env.NODECG_BASIC_USERS) {
+    const allowedUsers = env.NODECG_BASIC_USERS.split(",")
       .map((userPassword) => {
         const [username, password] = userPassword.split(":");
         if (username.length > 0 && password.length > 0)
@@ -72,20 +38,16 @@ if (!config.developer) {
   }
 
   // Twitch auth
-  if (
-    process.env.NODECG_TWITCH_CLIENT_ID &&
-    process.env.NODECG_TWITCH_CLIENT_SECRET &&
-    process.env.NODECG_TWITCH_ALLOWED_USERNAMES
-  ) {
-    const allowedUsernames = process.env.NODECG_TWITCH_ALLOWED_USERNAMES.split(",").filter((id) => id.length > 0);
+  if (env.NODECG_TWITCH_CLIENT_ID && env.NODECG_TWITCH_CLIENT_SECRET && env.NODECG_TWITCH_ALLOWED_USERNAMES) {
+    const allowedUsernames = env.NODECG_TWITCH_ALLOWED_USERNAMES.split(",").filter((id) => id.length > 0);
 
     if (allowedUsernames.length > 0) {
       console.info("Twitch auth enabled");
       config.login.enabled = true;
       config.login.twitch = {
         enabled: true,
-        clientID: process.env.NODECG_TWITCH_CLIENT_ID,
-        clientSecret: process.env.NODECG_TWITCH_CLIENT_SECRET,
+        clientID: env.NODECG_TWITCH_CLIENT_ID,
+        clientSecret: env.NODECG_TWITCH_CLIENT_SECRET,
         scope: "user_read",
         allowedUsernames,
       };
@@ -93,20 +55,16 @@ if (!config.developer) {
   }
 
   // Discord auth
-  if (
-    process.env.NODECG_DISCORD_CLIENT_ID &&
-    process.env.NODECG_DISCORD_CLIENT_SECRET &&
-    process.env.NODECG_DISCORD_ALLOWED_USERNAMES
-  ) {
-    const allowedUsernames = process.env.NODECG_DISCORD_ALLOWED_USERNAMES.split(",").filter((id) => id.length > 0);
+  if (env.NODECG_DISCORD_CLIENT_ID && env.NODECG_DISCORD_CLIENT_SECRET && env.NODECG_DISCORD_ALLOWED_USERNAMES) {
+    const allowedUsernames = env.NODECG_DISCORD_ALLOWED_USERNAMES.split(",").filter((id) => id.length > 0);
 
     if (allowedUsernames.length > 0) {
       console.info("Discord auth enabled");
       config.login.enabled = true;
       config.login.discord = {
         enabled: true,
-        clientID: process.env.NODECG_DISCORD_CLIENT_ID,
-        clientSecret: process.env.NODECG_DISCORD_CLIENT_SECRET,
+        clientID: env.NODECG_DISCORD_CLIENT_ID,
+        clientSecret: env.NODECG_DISCORD_CLIENT_SECRET,
         scope: "identify",
         allowedUsernames,
       };
@@ -114,21 +72,21 @@ if (!config.developer) {
   }
 
   // Steam auth
-  if (process.env.NODECG_STEAM_API_KEY && process.env.NODECG_STEAM_ALLOWED_IDS) {
-    const allowedIds = process.env.NODECG_STEAM_ALLOWED_IDS.split(",").filter((id) => id.length > 0);
+  if (env.NODECG_STEAM_API_KEY && env.NODECG_STEAM_ALLOWED_IDS) {
+    const allowedIds = env.NODECG_STEAM_ALLOWED_IDS.split(",").filter((id) => id.length > 0);
 
     if (allowedIds.length > 0) {
       config.login.enabled = true;
       config.login.steam = {
         enabled: true,
-        apiKey: process.env.NODECG_STEAM_API_KEY,
+        apiKey: env.NODECG_STEAM_API_KEY,
         allowedIds,
       };
     }
   }
 }
 
-if (process.env.NODECG_PORT) {
-  config.port = process.env.NODECG_PORT;
+if (env.NODECG_PORT) {
+  config.port = env.NODECG_PORT ?? 9090;
 }
 module.exports = config;
