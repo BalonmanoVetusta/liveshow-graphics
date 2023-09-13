e2d1d86ef31601ad9345f9f700a85cf522be32ff
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-13 16:00:36 +0200

    Change time directly

--- src/dashboard/shieldsconfig/src/components/advertising-config.tsx
+++ src/dashboard/shieldsconfig/src/components/advertising-config.tsx
@@ -1,16 +1,10 @@
 import { useGraphicsReplicant } from "hooks/replicants/use-graphics-replicant";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export function AdvertisingConfig() {
-  const [initial, setInitial] = useState(true);
   const { setGraphics, advertising, advertisingTime = 0 } = useGraphicsReplicant();
-  const [time, setTime] = useState(advertisingTime);
 
-  useEffect(() => {
-    if (!initial) setGraphics({ advertisingTime: time });
-    setInitial(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [time]);
+  useEffect(() => {}, [advertisingTime]);
 
   return (
     <fieldset>
@@ -22,9 +16,9 @@ export function AdvertisingConfig() {
         max={60}
         onChange={(event) => {
           event.preventDefault();
-          setTime(Number(event.target.value));
+          setGraphics({ advertisingTime: parseInt(event.target.value) });
         }}
-        value={time}
+        value={advertisingTime}
       />
       <button
         onClick={(event) => {
