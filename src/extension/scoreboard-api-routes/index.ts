44c0c3b4a7740d1cd3ff79bc8424a4509b09a03e
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-10-01 17:49:27 +0200

    Fix duplicated message when initilizing

--- src/extension/scoreboard-api-routes/index.ts
+++ src/extension/scoreboard-api-routes/index.ts
@@ -2,7 +2,5 @@ import type NodeCG from "@nodecg/types";
 import { handleScoreboardApiRoutes } from "./handle-scoreboard-api-routes";
 
 export async function scoreboardApiRoutes(nodecg: NodeCG.ServerAPI): Promise<void> {
-  nodecg.log.info("Initializing scoreboard-api-routes");
   handleScoreboardApiRoutes(nodecg);
-  nodecg.log.info("scoreboard-api-routes initialized");
 }
