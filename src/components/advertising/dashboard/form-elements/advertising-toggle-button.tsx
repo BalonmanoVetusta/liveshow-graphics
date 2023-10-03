d314ced20913d0398cfe68b54c8841885e0bc44f
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-10-03 17:40:13 +0200

    Remove non needed code

--- src/components/advertising/dashboard/form-elements/advertising-toggle-button.tsx
+++ src/components/advertising/dashboard/form-elements/advertising-toggle-button.tsx
@@ -1,14 +1,11 @@
 import { useAdvertisingReplicant } from "hooks/replicants/use-advertising-replicant";
-import { useEffect } from "react";
 
 export function ShowAdvertisingInput({
   labelShow = "Show advertising",
   labelHide = "Hide advertising",
 }: { labelShow?: string; labelHide?: string } = {}) {
   const { show, setAdvertising } = useAdvertisingReplicant();
-  useEffect(() => {
-    console.log({ show });
-  }, [show]);
+
   return (
     <>
       <button
